4b5fbc5fefe024c1935d0bd70faa7b2129c93d95
Jason Lyn <jason.lyn.lol@gmail.com>
2017-10-23 21:19:56 +1100

    Create tClient.py

---
+++ tClient.py
@@ -0,0 +1,14 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = ('localhost', 50000)
+s.connect(server_address)
+
+message = raw_input("---->")
+while True:
+  s.send(message)
+  data = s.recv(1024)
+  print "Receiving from server: " + str(data)
+  message = raw_input("--->")
+  
+s.close()

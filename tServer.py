66c430ebc5d48d250903fd8493128fb955021a96
Jason Lyn <jason.lyn.lol@gmail.com>
2017-10-23 21:15:07 +1100

    Create tServer.py

---
+++ tServer.py
@@ -0,0 +1,17 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = ('localhost', 50000)
+
+s.bind(server_address)
+s.listen(1)
+connection, address = s.accept()
+print "Connection from: " + str(address)
+
+while True:
+  data = s.recv(1024)
+  print "Receiving data from: " + str(data)
+  connection.send(data)
+  
+connection.close()
+  
